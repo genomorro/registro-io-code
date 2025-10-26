382af9929faeb766e4f10dceb7771403f0896b4f
genomorro <edgar_uriel84@genomorro.name>
2025-10-26 01:12:28 -0600

    Fix dropdown menu

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,11 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
+import './bootstrap.js';
+import 'bootstrap/bootstrap.index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
