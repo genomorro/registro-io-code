62e82cc6a92beade80474508f248ab78d7833f41
genomorro <edgar_uriel84@genomorro.name>
2025-10-29 10:06:34 -0600

    Redirect to default locale

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,14 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'app_index')]
+    #[Route(path: '/', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('index.html.twig');
+    }
+
+    public function redirectToLocale(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_index', ['_locale' => 'es']);
     }
 }
