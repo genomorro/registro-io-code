386ada20a5dbfe894ea3190d0ffe37036aa2eb2e
genomorro <edgar_uriel84@genomorro.name>
2025-11-29 16:51:51 -0600

    Change choice label for a custom string

--- src/Form/PatientAutocompleteField.php
+++ src/Form/PatientAutocompleteField.php
@@ -18,8 +18,14 @@ class PatientAutocompleteField extends AbstractType
             'label' => 'Patient',
             'class' => Patient::class,
             'placeholder' => 'Search by name or by file',
-            'choice_label' => 'name',
-            'searchable_fields' => ['name', 'file'],
+            'choice_label' => function(Patient $patient)
+	    {
+		return sprintf(
+		    '(%s) %s',
+		    $patient->getFile(),
+		    $patient->getName());
+	    },
+            'searchable_fields' => ['file', 'name'],
             'extra_options' => [],
         ]);
 
