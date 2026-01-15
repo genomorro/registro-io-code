661d300b2e105e7abeea11d6d4207c4becc8b637
genomorro <edgar_uriel84@genomorro.name>
2026-01-15 12:29:55 -0600

    Implement KNP Paginator in Stakeholder Entity

--- src/Repository/StakeholderRepository.php
+++ src/Repository/StakeholderRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Stakeholder;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -16,28 +17,19 @@ class StakeholderRepository extends ServiceEntityRepository
         parent::__construct($registry, Stakeholder::class);
     }
 
-    //    /**
-    //     * @return Stakeholder[] Returns an array of Stakeholder objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    /**
+     * @return Query
+     */
+    public function paginateStakeholder(string $filter = null): Query
+    {
+        $query = $this->createQueryBuilder('s')
+		      ->orderBy('s.id', 'ASC');
 
-    //    public function findOneBySomeField($value): ?Stakeholder
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+        if ($filter) {
+            $query->andWhere('s.name LIKE :filter OR s.tag LIKE :filter')
+                  ->setParameter('filter', '%' . $filter . '%');
+        }
+
+        return $query->getQuery();
+    }
 }
