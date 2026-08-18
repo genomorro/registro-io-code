34420d0cead8d2198b7424bb1c19f40f328eef8a
genomorro <edgar_uriel84@genomorro.name>
2026-08-18 08:29:32 -0600

    Delete examples and fix deprecation

--- src/Repository/EmployeeRepository.php
+++ src/Repository/EmployeeRepository.php
@@ -18,35 +18,10 @@ class EmployeeRepository extends ServiceEntityRepository
         parent::__construct($registry, Employee::class);
     }
 
-    //    /**
-    //     * @return Employee[] Returns an array of Employee objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('e')
-    //            ->andWhere('e.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('e.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Employee
-    //    {
-    //        return $this->createQueryBuilder('e')
-    //            ->andWhere('e.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
-
     /**
      * @return Query
      */
-    public function paginateEmployee(string $filter = null): Query
+    public function paginateEmployee(?string $filter = null): Query
     {
 	$query = $this->createQueryBuilder('e')
 		      ->leftJoin('e.area', 'a')
